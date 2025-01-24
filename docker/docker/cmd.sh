2df930d5c04008f88a5b69cbd8cda67813d433fa
Amir <>
2025-01-24 19:05:43 +0330

    add roll to k8s

--- docker/docker/cmd.sh
+++ docker/docker/cmd.sh
@@ -3,3 +3,7 @@ kubectl exec spark-master-88f78d74c-s6stx -it -- \pyspark --conf spark.driver.bi
 kubectl apply -f spark-job.yaml
 kubectl get jobs
 kubectl logs job/spark-stream-processing
+
+# roll
+kubectl apply -f role.yaml
+kubectl apply -f rolebinding.yaml
