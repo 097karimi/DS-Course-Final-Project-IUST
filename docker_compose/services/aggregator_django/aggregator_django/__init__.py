8e9476244c3418e03fad91447c2f4f530d143fc6
Amir <>
2025-01-25 15:36:04 +0330

    clean code

--- docker_compose/services/aggregator_django/aggregator_django/__init__.py
+++
@@ -1,3 +0,0 @@
-frdefault_app_config = 'aggregator_django.apps.AggregatorDjangoConfig'
-
-from . import signals
--- docker_compose/services/aggregator_django/aggregator_django/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class AggregatorDjangoConfig(AppConfig):
-    name = 'aggregator_django'
-
-    def ready(self):
-        import aggregator_django.signals
